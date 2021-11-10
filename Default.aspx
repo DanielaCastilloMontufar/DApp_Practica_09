846042570200064eeba62a15bead21fad0a3f2de
DanielaCastilloMontufar <82785812+DanielaCastilloMontufar@users.noreply.github.com>
2021-11-10 10:58:07 -0500

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="LinqObjects._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    
+
+    <p>
+        &nbsp;</p>
+    <p>
+        Total Peliculas:
+        <asp:TextBox ID="TotalMovies" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Maximo Runtime:
+        <asp:TextBox ID="LongestRunTime" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Minimo Runtime:
+        <asp:TextBox ID="ShortestRunTime" runat="server"></asp:TextBox>
+    </p>
+    <p>
+        Promedio Runtime:
+        <asp:TextBox ID="AverageRunTime" runat="server"></asp:TextBox>
+    </p>
+
+    
+
+</asp:Content>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LinqObjects
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            {
+                var movies = GetMovies(); var genres = GetGenres();
+                var query = (from m in movies
+                             join g in genres on m.Genre equals g.ID
+                             select
+new { m.Title, g.Name }).Skip(1).Take(2);
+                this.GridView1.DataSource = query; this.GridView1.DataBind();
+            }
+        }
+
+        public List<Movie> GetMovies()
+        {
+            return new List<Movie> {
+            new Movie { Title="Shrek", Director="Andrew Adamson", Genre=0,
+            ReleaseDate=DateTime.Parse("2001/05/16"), RunTime=89 },
+            new Movie { Title="Fletch", Director="Michael Ritchie", Genre=0,
+            ReleaseDate=DateTime.Parse("1985/05/31"), RunTime=96 },
+            new Movie { Title="Casablanca", Director="Michael Curtiz", Genre=1,
+            ReleaseDate=DateTime.Parse("1942/01/01"), RunTime=102 },
+            new Movie { Title="Batman", Director="Tim Burton", Genre=1,
+            ReleaseDate=DateTime.Parse("1989/06/23"), RunTime=126 },
+            new Movie { Title="Batman 2", Director="Tim Burton", Genre=0,
+            ReleaseDate=DateTime.Parse("1999/06/23"), RunTime=126 }
+            };
+        }
+
+        public List<Genre> GetGenres()
+        {
+            return new List<Genre> { new Genre { ID=0, Name="Comedy" } , new Genre { ID=1, Name="Drama" } , new Genre { ID=2, Name="Action" }
+ };
+        }
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class LastNameComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            var director1LastName = x.Substring(x.LastIndexOf(' ')); var
+         director2LastName = y.Substring(y.LastIndexOf(' ')); return
+         director1LastName.CompareTo(director2LastName);
+        }
+    }
+    }
